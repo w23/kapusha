b01d72b4dcb8a886a1228103a42095d7c31315f0
Ivan 'w23' Avdeev <me@w23.ru>
2012-12-23 15:14:21 +0700

    Makefile: release build by default

--- common.mk
+++ common.mk
@@ -1,7 +1,13 @@
 CXX=g++
 LD=g++
-CXXFLAGS=-Wall -Werror -fno-exceptions -fno-rtti `pkg-config --cflags sdl` -g -DDEBUG=1 -I$(ROOT)
+CXXFLAGS=-Wall -Werror -fno-exceptions -fno-rtti `pkg-config --cflags sdl` -I$(ROOT)
 LDFLAGS=`pkg-config --libs sdl` -lGL -lm
+
+ifeq ($(DEBUG),1)
+CXXFLAGS += -g -DDEBUG=1
+else
+CXXFLAGS += -Os -fomit-frame-pointer
+endif
 
 .SUFFIXES: .cpp .o
 
