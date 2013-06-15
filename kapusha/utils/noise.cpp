6f61b5bfacc81e5be2f291d72b7bbc95bd11ff3c
Ivan 'w23' Avdeev <me@w23.ru>
2013-06-16 00:35:01 +0700

    utils: более точнее, посоны

--- kapusha/utils/noise.cpp
+++ kapusha/utils/noise.cpp
@@ -4,7 +4,7 @@ namespace kapusha {
   float hash(int x) {
     // stolen from teh internets!
     x = (x<<13) ^ x;
-    return (1.0 - ((x*(x*x*15731+789221)+1376312589)&0x7fffffff)/1073741824.0);
+    return (1.f - ((x*(x*x*15731+789221)+1376312589)&0x7fffffff)/1073741824.f);
   }
   float hash(vec2i p) { return hash(p.x + p.y * 27644437); }
   float lerp_noise(vec2f x) {
