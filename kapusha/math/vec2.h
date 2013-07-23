a020c828275e00e7407b414070eea35ca86d2483
Ivan 'w23' Avdeev <me@w23.ru>
2013-07-24 01:08:28 +0700

    math: default constructor for vec2

--- kapusha/math/vec2.h
+++ kapusha/math/vec2.h
@@ -8,7 +8,7 @@ namespace kapusha {
     typedef T type;
     T x, y;
 
-    inline vec2() {}
+    inline vec2() = default;
     inline explicit vec2(T v) : x(v), y(v) {}
     inline vec2(T _x, T _y) : x(_x), y(_y) {}
     template <typename R> inline vec2(const vec2<R>& v)
