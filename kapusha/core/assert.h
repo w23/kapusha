ed8c290bb7fb88ddc38da90a70d6587a62facd87
Ivan 'w23' Avdeev <me@w23.ru>
2013-08-28 01:36:00 +0700

    linux: fix build

--- kapusha/core/assert.h
+++ kapusha/core/assert.h
@@ -1,13 +1,14 @@
 #pragma once
+
 namespace kapusha {
-  void assert_perror(const char *expr, const char *file, int line, const char *func);
+  void __assert_perror(const char *expr, const char *file, int line, const char *func);
 }
 
 #if DEBUG
 #ifndef __PRETTY_FUNCTION__
 #define __PRETTY_FUNCTION__ __FUNCTION__
 #endif
-#define KP_ASSERT(expr) if(!(expr)) ::kapusha::assert_perror(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#define KP_ASSERT(expr) if(!(expr)) ::kapusha::__assert_perror(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__)
 #define KP_ENSURE(expr) KP_ASSERT(expr)
 #else
 #define KP_ASSERT(expr) {}
