ef094ec4a6d15435aa6a8dea7c70d0d9a51d0139
Ivan 'w23' Avdeev <me@w23.ru>
2014-11-05 22:46:36 +0600

    sys: remove old x11 impl

--- src/sys/x11_main.c
+++
@@ -1,9 +0,0 @@
-#include "x11_systems.h"
-
-int main(int argc, char *argv[]) {
-  KPsystems_x11_t x11;
-  KP_systems_x11_ctor(&x11);
-  int result = KP_main(argc, argv, &x11);
-  KP_systems_x11_dtor(&x11);
-  return result;
-}
